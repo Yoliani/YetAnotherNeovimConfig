9b68c9690bf2a8e5459a4ff9999e6cf302f981d2
Yoliani <edrayoca@outlook.es>
2021-07-21 19:48:01 -0500

    Implementacion de gitui en Fterm

--- lua/float-term.lua
+++ lua/float-term.lua
@@ -1,3 +1,4 @@
+--[[
 require'FTerm'.setup({
     dimensions  = {
         height = 0.8,
@@ -7,12 +8,27 @@ require'FTerm'.setup({
     },
     border = 'single' -- or 'double'
 })
+--]]
+local term = require("FTerm.terminal")
+
+local gitui = term:new():setup({
+    cmd = "gitui",
+    dimensions = {
+        height = 0.9,
+        width = 0.9
+    }
+})
+
+ -- Use this to toggle gitui in a floating terminal
+function _G.__fterm_gitui()
+    gitui:toggle()
+end
 
 -- Keybinding
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>', opts)
+map('n', '<A-i>', '<CMD>lua __fterm_gitui()<CR>', opts)
 map('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
 
 --[[

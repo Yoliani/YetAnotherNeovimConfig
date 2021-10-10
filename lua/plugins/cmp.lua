ea64eee26bb7145d845fa7715284cbf4c4df7c48
Yoliani <edrayoca@outlook.es>
2021-10-10 11:35:38 -0500

    commit

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,20 +1,6 @@
 local cmp = require("cmp")
-local lspkind = require("lspkind")
-
 vim.opt.completeopt = "menuone,noselect"
 local luasnip = require("luasnip")
-
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    return false
-  end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), "n", true)
-end
 
 if not luasnip then
   return
@@ -56,32 +42,34 @@ cmp.setup(
       end
     },
     mapping = {
-      ["<Tab>"] = cmp.mapping(
-        function(fallback)
-          if vim.fn.pumvisible() == 1 then
-            feedkey("<C-n>")
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-          end
-        end,
-        {"i", "s"}
-      ),
-      ["<S-Tab>"] = cmp.mapping(
-        function(fallback)
-          if vim.fn.pumvisible() == 1 then
-            feedkey("<C-p>")
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end,
-        {"i", "s"}
-      ),
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.close(),
+      ["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true
+      },
+      ["<Tab>"] = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif require("luasnip").expand_or_jumpable() then
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+        else
+          fallback()
+        end
+      end,
+      ["<S-Tab>"] = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif require("luasnip").jumpable(-1) then
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+        else
+          fallback()
+        end
+      end,
       ["<cr>"] = cmp.mapping.confirm({select = true})
     },
     formatting = {

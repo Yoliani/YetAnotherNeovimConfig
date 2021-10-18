04ba5bafe6540a59b1aca328931cf5cb6ca3349a
Yoliani <edrayoca@outlook.es>
2021-10-17 20:55:45 -0500

    Commit

--- init.lua
+++ init.lua
@@ -1,17 +1,18 @@
-local async
+--local async
 require "impatient"
-_G.tab = {}
+--_G.tab = {}
+--[[
 async =
   vim.loop.new_async(
   vim.schedule_wrap(
     function()
-      require("pluginList")
-      require("settings")
-      require("plugins")
-      require("mappings")
-      require("autocmds")
-    end
+         end
   )
 )
 
-async:send()
+--async:send()]]
+require("pluginList")
+require("settings")
+require("plugins")
+require("mappings")
+require("autocmds")
